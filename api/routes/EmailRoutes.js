e6579748d99c8fa88d67bd6afefd658c46af5717
Guilherme Catini <guilherme.catini@iv2.com.br>
2017-04-25 14:41:36 -0300

    ae

--- api/routes/EmailRoutes.js
+++ api/routes/EmailRoutes.js
@@ -1,28 +1,24 @@
 'use strict'
 
-const express = require('express');
-const router = express.Router();
-
-const nodemailer = require('nodemailer')
-
-router.post('/send', (req, res) => {
-
-    const data = req.body
-
-    const destinatario = data.destinatario
-    const assunto = data.assunto
-    const mensagem = data.mensagem
+function SendEmail(req, res, destinatario, assunto, mensagem) {
+     // const trp = nodemailer.createTransport({
+    //     host: 'mail.ita.locaweb.com.br',
+    //     port: '587',
+    //     secure: false,
+    //     tls: {
+    //         rejectUnauthorized: false
+    //     },
+    //     auth: {
+    //         user: 'guilherme.catini@iv2.com.br',
+    //         pass: '@Ggcr2305'
+    //     }
+    // })
 
     const trp = nodemailer.createTransport({
-        host: 'mail.ita.locaweb.com.br',
-        port: '587',
-        secure: false,
-        tls: {
-            rejectUnauthorized: false
-        },
+        service: 'Gmail',
         auth: {
-            user: 'guilherme.catini@iv2.com.br',
-            pass: '@Ggcr2305'
+            user: 'guilherme@catini.org',
+            pass: '4493Gui9750230590'
         }
     })
 
@@ -37,7 +33,32 @@ router.post('/send', (req, res) => {
         if (err) return res.json(err, 500)
             return res.json(data, 200)
     })
+}
 
+const express = require('express');
+const router = express.Router();
+
+const nodemailer = require('nodemailer')
+
+router.post('/send', (req, res) => {
+
+    const data = req.body
+
+    const destinatario = data.destinatario
+    const assunto = data.assunto
+    const mensagem = data.mensagem
+
+    SendEmail(req, res, destinatario, assunto, mensagem)
+
+})
+
+router.get('/send/:destinatario/:assunto/:mensagem', (req, res) => {
+
+    const destinatario = req.params.destinatario
+    const assunto = req.params.assunto
+    const mensagem = req.params.mensagem
+
+    SendEmail(req, res, destinatario, assunto, mensagem)
 
 })
 
